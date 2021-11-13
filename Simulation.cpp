def66bab6363aedae2984d99a9737137214b459f
Patrick James <pjjames@mymail.mines.edu>
2021-11-13 12:48:03 -0700

    Event List

--- Simulation.cpp
+++ Simulation.cpp
@@ -3,11 +3,13 @@
 //
 
 #include "Simulation.h"
+#include "Event.h"
 #include <iostream>
-
+#include <queue>
 using namespace std;
 
 int main() {
+    priority_queue<Event> EventList;
     Welford W = Welford();
     W.v += 1;
     cout << W.v << endl;
