114452100f87f9f14153cee980f68640af200f20
Patrick James <pjjames@mymail.mines.edu>
2021-11-13 12:08:46 -0700

    cmake

---
+++ cmake-build-debug/CMakeFiles/CrosswalkSim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CrosswalkSim.dir/Simulation.cpp.obj"
+  "CrosswalkSim.exe"
+  "CrosswalkSim.exe.manifest"
+  "CrosswalkSim.pdb"
+  "libCrosswalkSim.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CrosswalkSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
